529353ae40032cd9f61e1515248ba254a24072e0
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 22:54:07 +0800

    Add files via upload

---
+++ pointers/pointers/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  pointers
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int *p=&a;
+        
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
+        
+    return 0;
+        
+}
